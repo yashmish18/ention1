53b584cbb30471896e9b7d1c9893b65344675685
Koustav <samaddarkoustav@gmail.com>
2025-02-09 12:12:47 +0530

    footer small changes

--- components/generic/TypingEffect.jsx
+++ components/generic/TypingEffect.jsx
@@ -14,7 +14,7 @@ export function TypingEffect({ text, className }) {
           key={index}
           initial={{ opacity: 0 }}
           animate={isInView ? { opacity: 1 } : {}}
-          transition={{ duration: 0.5, delay: index * 0.1 }}
+          transition={{ duration: 0.7, delay: index * 0.1 }}
         >
           {letter}
         </motion.span>
